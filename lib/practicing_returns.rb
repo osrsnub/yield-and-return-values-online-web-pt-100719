8a93f95d77a2b775eaae22984acd57f422d08d14
Aaron Cherniavsky <aaron.b.cherniavsky@gmail.com>
2019-10-24 14:37:24 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,11 @@
-require 'pry'
-
-def hello(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i += 1
-  end
-end
-
+    def hello(array)
+      i = 0
+      collection = []
+      while i < array.length
+        collection << yield(array[i])
+        i += 1
+      end
+      collection
+    end
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
